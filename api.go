737d4f93cf8e2703aefc0070aa8a1384999b6459
akshay-sharma1 <aksharma@seas.upenn.edu>
2022-12-15 20:00:52 -0500

    fix problem with unsplash image links

--- api.go
+++ api.go
@@ -30,8 +30,7 @@ type Genre struct {
 }
 
 func GetMoodMetadata() Mood {
-	// TODO: change image links to proper source
-	BASE_URL := "https://source.unsplash.com"
+	BASE_URL := "https://source.unsplash.com/"
 
 	image_ids := []string{"dWIVg59BVXY", "fnztlIb52gU", "s9CC2SKySJM", "zfPOelmDc-M"}
 	image_urls := make([]string, 4)
@@ -41,11 +40,6 @@ func GetMoodMetadata() Mood {
 	}
 
 	identifier_list := []string{"Chill", "Mood Booster", "Deep Focus", "Workout"}
-	identifier_map := make(map[string]string)
-
-	for i := 0; i < 4; i++ {
-		identifier_map[identifier_list[i]] = image_urls[i]
-	}
 
 	new_mood := Mood{
 		Identifier: identifier_list,
